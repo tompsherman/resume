46bbbbbe89e588277b097bd09a7866a8ac0316ae
unknown <tompsherman@gmail.com>
2021-02-14 16:54:02 -0500

    initial scaffolding

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>this is tom's resume app</h1>
+    </div>
+  );
+};
+
+export default App;
